2cd148ef80594e46efb800de3364130aa369c9f2
GAierken <g.aierken@gmail.com>
2020-06-14 12:18:08 -0400

    user serializer removed password

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :password_digest, :username, :email, :average_volunteer_rating, :credit, :is_volunteer
+  attributes :id, :first_name, :last_name, :username, :email, :average_volunteer_rating, :credit, :is_volunteer
   has_many :volunteer_appointments, foreign_key: :client_id, class_name: 'Appointment', dependent: :destroy
   has_many :volunteers, through: :volunteer_appointments
 
